64110ed856df7ab418031824bb606f906aba3d0b
Shawn <96506264+shawnjoseph1@users.noreply.github.com>
2022-07-25 19:29:52 -0400

    fixed issue with events over lapping

--- event-finder/assets/script.js
+++ event-finder/assets/script.js
@@ -23,31 +23,30 @@ function getUserData(url) {
       theresults = JSON.parse(results)
       console.log(results)
 
-            $('#event1').append('<div>');
-            for (var i = 0; i < 1; i++) {
-              $('#event1').append('<h2>' + data._embedded.events[i].name + '</h2>');
-              $('#event1').append('<h4>' + data._embedded.events[i]._embedded.venues[0].name + '</h4>');
-              $('#event1').append('<h3>' + data._embedded.events[i].dates.start.localDate + '</h3>');
-              $('#event1').append('<img src="' + data._embedded.events[i].images[1].url + '">');
-              }
-            $('#event1').append('</div>');
-      
-            $('#event2').append('<div>');
-            for (var i = 1; i < 2; i++) {
-              $('#event2').append('<h2>' + data._embedded.events[i].name + '</h2>');
-              $('#event2').append('<h4>' + data._embedded.events[i]._embedded.venues[0].name + '</h4>');
-              $('#event2').append('<h3>' + data._embedded.events[i].dates.start.localDate + '</h3>');
-              $('#event2').append('<img src="' + data._embedded.events[i].images[1].url + '">');
-              }
-            $('#event2').append('</div>');
-           
-            $('#event3').append('<div>');
-            for (var i = 2; i < 3; i++) {
-              $('#event3').append('<h2>' + data._embedded.events[i].name + '</h2>');
-              $('#event3').append('<h4>' + data._embedded.events[i]._embedded.venues[0].name + '</h4>');
-              $('#event3').append('<h3>' + data._embedded.events[i].dates.start.localDate + '</h3>');
-              $('#event3').append('<img src="' + data._embedded.events[i].images[1].url + '">');
-              }
-            $('#event3').append('</div>');
+              // delete previous results once the user submits a new search
+              $('#event1').empty();
+              $('#event2').empty();
+              $('#event3').empty();
+    
+              // append new results
+              for (var i = 0; i < 1; i++) {
+                $('#event1').append('<h2>' + theresults._embedded.events[i].name + '</h2>');
+                $('#event1').append('<h3>' + theresults._embedded.events[i]._embedded.venues[0].name + '</h3>');
+                $('#event1').append('<h3>' + theresults._embedded.events[i].dates.start.localDate + '</h3>');
+                $('#event1').append('<img src="' + theresults._embedded.events[i].images[1].url + '">');
+                }
+              for (var i = 1; i < 2; i++) {
+                $('#event2').append('<h2>' + theresults._embedded.events[i].name + '</h2>');
+                $('#event2').append('<h3>' + theresults._embedded.events[i]._embedded.venues[0].name + '</h3>');
+                $('#event2').append('<h3>' + theresults._embedded.events[i].dates.start.localDate + '</h3>');
+                $('#event2').append('<img src="' + theresults._embedded.events[i].images[1].url + '">');
+                }
+              for (var i = 2; i < 3; i++) {
+                $('#event3').append('<h2>' + theresults._embedded.events[i].name + '</h2>');
+                $('#event3').append('<h3>' + theresults._embedded.events[i]._embedded.venues[0].name + '</h3>');
+                $('#event3').append('<h3>' + theresults._embedded.events[i].dates.start.localDate + '</h3>');
+                $('#event3').append('<img src="' + theresults._embedded.events[i].images[1].url + '">');
+                }         
       }); 
+      
    }
